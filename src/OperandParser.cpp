f6405b8d055cc0760988299401a1556d412ffbf5
Omar Shawky <omaralgamil@gmail.com>
2020-05-14 21:50:43 +0200

    Update OperandParser.cpp

--- src/OperandParser.cpp
+++ src/OperandParser.cpp
@@ -24,12 +24,14 @@ long long OperandParser::parseOperand(string &operand, int &locationCounter, Sym
                 i++;
             }
             address += symbolTable.get(temp) * sign;
+            sign=1;
         }else if(isdigit(operand[i])){
             while (i < operand.size() && isdigit(operand[i])) {
                 temp += operand[i];
                 i++;
             }
             address += std::atoi(temp.c_str()) * sign;
+            sign=1;
         }
     }
     return address;
